0f1fcf00064532eef971109ab28833a16884822b
Rafaabenhassine <rafaabenhassine1@gmail.com>
2023-10-08 22:32:56 +0100

    update

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,11 +1,75 @@
 import React from 'react'
-
+import "./Login.css"
 const Login = () => {
   return (
-    <div>
-        <h1>Login</h1>
+    <div className="login-box">
+  <h2>Login</h2>
+  <form>
+    <div className="user-box">
+    <input type="text" name="" required=""></input>
+
+      <label>Username</label>
     </div>
+    <div className="user-box">
+      <input type="password" name="" required=""></input>
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+    
+  </form>
+
+  
+</div>
   )
 }
 
 export default Login
+
+
+
+
+
+
+// import React from 'react'
+// import "./Login.css"
+// const Login = () => {
+//   return (
+//     <div class="login">
+// 	<h1>Login</h1>
+//     <form method="post">
+//     	<input type="text" name="u" placeholder="Username" required="required" />
+//         <input type="password" name="p" placeholder="Password" required="required" />
+//         <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+//     </form>
+// </div>
+//   )
+// }
+
+// export default Login
+
+{/* <div className="login-box">
+  <h2>Login</h2>
+  <form>
+    <div className="user-box">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div className="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+</div> */}
--- src/Page/Register.js
+++ src/Page/Register.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Register.css';
 
 const Register = () => {
   return (
-    <div>
-        <h1>Register</h1>
-    </div>
-  )
-}
+    <div className="testbox">
+      <h1>Registration</h1>
 
-export default Register
+      <form action="/">
+        <div className="accounttype">
+          <input type="radio" value="None" id="radioOne" name="account" checked />
+          <label htmlFor="radioOne" className="radio">Personal</label>
+          <input type="radio" value="None" id="radioTwo" name="account" />
+          <label htmlFor="radioTwo" className="radio">Company</label>
+        </div>
+        
+        <label htmlFor="email"><i className="icon-envelope"></i></label>
+        <input type="text" name="email" id="email" placeholder="Email" required />
+        <label htmlFor="name"><i className="icon-user"></i></label>
+        <input type="text" name="name" id="name" placeholder="Name" required />
+        <label htmlFor="password"><i className="icon-shield"></i></label>
+        <input type="password" name="password" id="password" placeholder="Password" required />
+        <div className="gender">
+          <input type="radio" value="None" id="male" name="gender" checked />
+          <label htmlFor="male" className="radio">Male</label>
+          <input type="radio" value="None" id="female" name="gender" />
+          <label htmlFor="female" className="radio">Female</label>
+        </div>
+        <p>By clicking Register, you agree to our <a href="#">terms and conditions</a>.</p>
+        <button type='submit' className='button'>Register</button>
+      </form>
+      
+    </div>
+  );
+};
+
+export default Register;
